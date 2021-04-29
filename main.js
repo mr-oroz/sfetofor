822ea879c5853b06a9774d4ab9186b9a329ccaac
mr-oroz <oroz7307@gmail.comm>
2021-04-29 23:10:01 +0600

    first commit

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 const time = document.body.children[1].children[1].children[0]
 const time2 = document.body.children[0].children[1].children[0]
-// const green = document.getElementById('green')
-// const green_2 = document.getElementById('green_2')
-// const red = document.getElementById('red')
-// const red_2 = document.getElementById('red_2')
+const green = document.getElementById('green')
+const green_2 = document.getElementById('green_2')
+const red = document.getElementById('red')
+const red_2 = document.getElementById('red_2')
 console.dir(green)
 let second = 15;
 let found = true
@@ -13,10 +13,10 @@ const myFunc = () => {
     time2.innerText = second
     if (second < 1) {
         second = 15;
-        // red.style.backgroundColor = 'red'
-        // green.style.backgroundColor = 'green'
-        // red_2.style.backgroundColor = 'red'
-        // green_2.style.backgroundColor = 'green'
+        red.classList.toggle("red")
+        green.classList.toggle("green")
+        red_2.classList.toggle("red")
+        green_2.classList.toggle("green")
     }
 }
 
