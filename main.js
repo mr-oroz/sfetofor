c7a79288522c463b8ccda759cf659e93154f4e17
mr-oroz <oroz7307@gmail.comm>
2021-07-26 13:41:17 +0600

    add setTimout

--- main.js
+++ main.js
@@ -1,23 +1,39 @@
-const time = document.body.children[1].children[1].children[0]
-const time2 = document.body.children[0].children[1].children[0]
-const green = document.getElementById('green')
-const green_2 = document.getElementById('green_2')
-const red = document.getElementById('red')
-const red_2 = document.getElementById('red_2')
-console.dir(green)
-let second = 15;
-let found = true
-const myFunc = () => {
-    second--;
-    time.innerText = second
-    time2.innerText = second
-    if (second < 1) {
-        second = 15;
-        red.classList.toggle("red")
-        green.classList.toggle("green")
-        red_2.classList.toggle("red")
-        green_2.classList.toggle("green")
-    }
-}
 
-setInterval(myFunc, 1000);
+const green = document.getElementById('green')
+const green2 = document.getElementById('green2')
+const red = document.getElementById('red')
+const red2 = document.getElementById('red2')
+const spans = document.getElementsByTagName('span');
+const yellow = document.getElementsByClassName('yellow')[0]
+let second = 10;
+let show = 0;
+
+
+setInterval(() => {
+    spans[0].innerText = second;
+    spans[1].innerText = second;
+    second--;
+    setTimeout(() => {
+        if (second < 5) {
+            spans[0].style.display = 'none';
+            spans[1].style.display = 'none';
+        }
+    }, 300);
+    spans[0].style.display = 'block';
+    spans[1].style.display = 'block';
+    if (second === -1) {
+        second = 10
+        show = show === 1 ? 2 : 1
+    }
+    if (show === 1) {
+        red.style.backgroundColor = 'red';
+        green.style.backgroundColor = 'black';
+        red2.style.backgroundColor = 'black';
+        green2.style.backgroundColor = 'green';
+    } else if (show === 2) {
+        red.style.backgroundColor = 'black';
+        green.style.backgroundColor = 'green';
+        red2.style.backgroundColor = 'red';
+        green2.style.backgroundColor = 'black';
+    }
+}, 1000);
